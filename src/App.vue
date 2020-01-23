21858e927f099e72e6dbdb9fd57b1109a6e9c963
Fares A. Akel C <f.antonio.akel@gmail.com>
2020-01-23 02:36:48 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BancorWidget tokenReceive="OMNIS" />
+    <DataTable :transactions="transactions" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import toVue from 'svelte-adapter/vue';
+import BancorConversionWidget from 'bancor-conversion-widget';
+import Eth from 'ethjs-query';
+import EthFilter from 'ethjs-filter';
+import DataTable from './components/DataTable.vue';
+
+let provider;
+let eth;
+let filters;
+
+const baseStyle = {
+  width: '50%',
+  margin: 'auto'
+};
 
 export default {
+  data() {
+    return {
+      txFilter: null,
+      transactions: []
+    };
+  },
   name: 'app',
   components: {
-    HelloWorld,
+    BancorWidget: toVue(BancorConversionWidget, baseStyle, 'div'),
+    DataTable
   },
+  beforeMount() {
+    if (typeof window.ethereum !== 'undefined') {
+      provider = window.ethereum;
+      eth = new Eth(provider);
+      filters = new EthFilter(eth);
+
+      this.txFilter = new filters.Filter({ delay: 300 })
+        .new({
+          fromBlock: 9269547,
+          toBlock: 'latest',
+          address: '0x8e748dc7b9dab8bf9ef4b84c5f38a6ae954096e0',
+          topics: ['0x276856b36cbc45526a0ba64f44611557a2a8b68662c5388e9fe6d72e86e1c8cb', null]
+        })
+        .then(res => {
+          eth.getFilterLogs(res).then(result => {
+            this.transactions = [];
+            result.forEach(element => {
+              const tx = {
+                sendToken: `0x${element.topics[1].substring(26, 66)}`,
+                sendAmount: parseInt(`0x${element.data.substring(2, 66)}`, 16) / 1e18,
+                receiveToken: `0x${element.topics[2].substring(26, 66)}`,
+                receiveAmount: parseInt(`0x${element.data.substring(66, 130)}`, 16) / 1e18,
+                block: element.blockNumber.toString()
+              };
+              this.transactions.push(tx);
+            });
+          });
+        })
+        .catch();
+    }
+  }
 };
 </script>
 
---
+++ src/components/DataTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="transactions"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Send Token',
+          sortable: false,
+          value: 'sendToken'
+        },
+        {
+          text: 'Send Amount',
+          sortable: false,
+          value: 'sendAmount'
+        },
+        {
+          text: 'Receive Token',
+          sortable: false,
+          value: 'receiveToken'
+        },
+        {
+          text: 'Receive Amount',
+          sortable: false,
+          value: 'receiveAmount'
+        },
+        {
+          text: 'Block',
+          sortable: false,
+          value: 'block'
+        }
+      ]
+    };
+  },
+  props: ['transactions']
+};
+</script>
